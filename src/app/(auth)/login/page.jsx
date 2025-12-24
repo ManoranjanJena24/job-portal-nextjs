bfef08f8ce270b3a92916d9990e39088b990d024
Manoranjan Jena <manoranjanjenamitsy17@gmail.com>
2025-12-24 15:09:40 +0530

    created the login ui

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { loginUser } from "@/redux/action/user";
+import { ArrowRight } from "lucide-react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-const page = () => {
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+
+    dispatch(loginUser(formData));
+  };
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="mt-20 md:mt-5 z-0">
+      <div className="md:w-1/3 border border-gray-400 rounded-lg p-8 flex flex-col w-full relative shadow-md m-auto">
+        <h2 className="mb-4 text-3xl">
+          Login to <span className="text-red-500">HireHeven</span>
+        </h2>
 
-export default page
+        <form onSubmit={submitHandler} className="flex flex-col gap-4">
+          <div>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <div>
+            <Label>Password</Label>
+            <Input
+              type="password"
+              placeholder="Enter your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <Button className="flex justify-center items-center gap-2">
+            Login <ArrowRight size={18} />
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
